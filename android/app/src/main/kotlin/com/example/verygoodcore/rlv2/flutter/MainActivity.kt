d009e5a0489298766c407be1bef82a4cc6acede8
Matt C <catmarlos@icloud.com>
2024-09-14 21:08:20 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/rlv2/flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.rlv2_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:rlv2_flutter/app/app.dart';
+import 'package:rlv2_flutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
