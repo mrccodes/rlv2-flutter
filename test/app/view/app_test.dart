4c01be5cc5948de074f065d2876a660136af2b88
Matt C <catmarlos@icloud.com>
2024-09-22 15:24:31 -0700

    remove counter

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rlv2_flutter/app/app.dart';
-import 'package:rlv2_flutter/counter/counter.dart';
+import 'package:rlv2_flutter/dashboard/dashboard_page.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders DashboardPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(DashboardPage), findsOneWidget);
     });
   });
 }
