f2610e7a293a2e538dbd85983032a2d5af4378a1
Matt C <catmarlos@icloud.com>
2024-10-01 16:04:36 -0700

    fix tests

--- test/helpers/test_setup.dart
+++ test/helpers/test_setup.dart
@@ -2,5 +2,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> testSetup() async {
   // Mock environment variables for all tests
+  // ignore: avoid_print
+  print('Setting up tests...');
+
   dotenv.testLoad(fileInput: 'API_URL=https://api.example.com\n');
+
+   // ignore: avoid_print
+  print('Tests setup.');
 }
