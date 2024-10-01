d76c23f1ec3233ccf11eccd1f6dd7e1314372100
Matt C <catmarlos@icloud.com>
2024-10-01 16:18:27 -0700

    fix test?

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -3,8 +3,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:rlv2_flutter/app/app.dart';
 import 'package:rlv2_flutter/features/dashboard/dashboard_screen.dart';
 
+import '../../helpers/test_setup.dart';
+
 void main() {
   group('App', () {
+        setUpAll(() async {
+      await testSetup();
+    });
+
     testWidgets('renders DashboardScreen', (tester) async {
       await tester.pumpWidget(const ProviderScope(child: App()));
       expect(find.byType(DashboardScreen), findsOneWidget);
