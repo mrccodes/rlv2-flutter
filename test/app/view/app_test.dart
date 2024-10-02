ed231f5c45c6fcf12feb262fb7ba81fe48e92042
Matt C <catmarlos@icloud.com>
2024-10-02 11:23:57 -0700

    fix test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,13 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rlv2_flutter/app/app.dart';
-import 'package:rlv2_flutter/features/dashboard/dashboard_screen.dart';
+import 'package:rlv2_flutter/features/auth/screens/landing_screen.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders DashboardScreen', (tester) async {
+    testWidgets('renders Landing Screen', (tester) async {
       await tester.pumpWidget(const ProviderScope(child: App()));
-      expect(find.byType(DashboardScreen), findsOneWidget);
+      expect(find.byType(LandingScreen), findsOneWidget);
     });
   });
 }
