7c740584665e270e81ebb425ebc9dbd6980b1453
Matt C <catmarlos@icloud.com>
2024-10-05 14:10:41 -0700

    user settings repo file [wip]

--- lib/features/user/repositories/user_settings_repository.dart
+++ lib/features/user/repositories/user_settings_repository.dart
@@ -1,3 +1,23 @@
 import 'package:rlv2_flutter/core/services/api_service.dart';
+import 'package:rlv2_flutter/features/user/models/user_settings_model.dart';
 
-class UserSettingsRepository {}
+class UserSettingsRepository {
+  UserSettingsRepository({required this.apiService});
+  final ApiService apiService;
+
+  Future<UserSettings> getUserSettings({required String userId}) async {
+    final endpoint = '/userSettings/$userId';
+
+    try {
+      // ignore: inference_failure_on_function_invocation
+      final response = await apiService.getRequest<UserSettings>(
+        endpoint,
+        UserSettings.fromJson,
+      );
+
+      return response.data!;
+    } catch (e) {
+      throw Exception('Failed to fetch user settings data: $e');
+    }
+  }
+}
